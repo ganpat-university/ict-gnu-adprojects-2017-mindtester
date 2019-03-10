b8f1be67701fad754d74893b9ec2ea9a55b905bd
dhyanvipatel <47962295+dhyanvipatel@users.noreply.github.com>
2019-03-10 19:06:04 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>About us</title>
+</head>
+<body>
+<div  style="background-image:url(image/bg1.jpg); min-height:430px;">
+<h2 align="center" style="font-family:'typo'; color:#000066">About us</h2>
+<div style="font-size:14px">
+<h4 align="center" style="font-family:'typo'; color:#000066">Welcome to the About Us page of our website, here on this page you will find out information about our website.</br>
+
+
+<h2>About Mindtester</h2></br>
+Mindtester is a website that is formed, developed and created for the people preparing for different examinations .</br>
+Mind tester is a website to conduct online examinations. It save time as it allows number of students to give the exam at a time and display the results as the test gets over, so need to wait for the result. </br>
+ It is automatically  when the test gets over.
+</br>
+
+Recently we have also launched many educational android app on Google Playstore. Just search mindtester in Google Playstore and download all our apps. Happy Learning.</h4>
+</div>
+</div>
+</body>
+</html>
